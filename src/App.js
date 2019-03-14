f9710f6d089229d63a009ca8bca4cd5651fa8caf
bo dai <bdai9@myseneca.ca>
2019-03-14 00:13:02 -0400

    bc

--- src/App.js
+++ src/App.js
@@ -72,16 +72,6 @@ class DisplayQuote extends Component {
       Like: "No"
     })      
   } 
-/*likeListAdd(){
-    var cnt=true;
-    if(this.Like==="Yes") {
-      likelist.map((i)=>{
-        if(i.currentIdx==this.currentIdx) cnt=false;
-      });
-      if(cnt==true) likelist.push(this);
-    }  
-    console.log(likelist) ;
-  }*/
 
   likeListRemove(){
     this.setState({
@@ -101,7 +91,7 @@ class DisplayQuote extends Component {
             <button className="btn btn-block btn-primary" onClick={this.likeQuote}><i className="fa fa-thumbs-up"></i>Like</button>
           </Col>
           <Col xs={6} sm={5} md={5} lg={3}>
-          <a target="_blank" href={'https://twitter.com/intent/tweet/?text='+this.state.quote.text+
+          <a style={{ textDecoration: 'none' }} target="_blank" href={'https://twitter.com/intent/tweet/?text='+this.state.quote.text+
           ' - '+this.state.quote.author}>
           <button className="btn btn-block btn-info"><i className="fa fa-twitter"></i>Tweet</button></a>
           </Col>   
