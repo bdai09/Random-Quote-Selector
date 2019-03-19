066b441759011aa31ba2a730038580a4d264cefb
bo dai <bdai9@myseneca.ca>
2019-03-19 01:16:23 -0400

    Updates

---
+++ precache-manifest.0c64d46fcbb893164f3af428f01a85a8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Random-Quote-Selector/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "36b5247971373d7464b9",
+    "url": "/Random-Quote-Selector/static/css/main.f2b8e193.chunk.css"
+  },
+  {
+    "revision": "6d550671c3a813918490",
+    "url": "/Random-Quote-Selector/static/js/runtime~main.42333646.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Random-Quote-Selector/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "20370b38fe25477bce57",
+    "url": "/Random-Quote-Selector/static/js/2.b5179f57.chunk.js"
+  },
+  {
+    "revision": "36b5247971373d7464b9",
+    "url": "/Random-Quote-Selector/static/js/main.38cdd2f3.chunk.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Random-Quote-Selector/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Random-Quote-Selector/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Random-Quote-Selector/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "20370b38fe25477bce57",
+    "url": "/Random-Quote-Selector/static/css/2.79c7a969.chunk.css"
+  },
+  {
+    "revision": "5ee472dc8ce15949a372f56fabfc78ab",
+    "url": "/Random-Quote-Selector/index.html"
+  }
+];
